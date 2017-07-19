01f45e8444150ef7f124e7d8d8303d424fc4b69d
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-07-19 11:51:10 -0500

    added models for group projects and random useful links

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Main from '../components/Main';
-import Manager from '../components/Manager';
 
 // Export the Routes
 module.exports = (
@@ -11,7 +10,6 @@ module.exports = (
   <Router>
     <div>
       <Route path="/" component={Main} />
-      <Route path="/Manager" component={Manager} />
     </div>
   </Router>
 );
--- server.js
+++ server.js
@@ -9,11 +9,12 @@ var camprdb = require("./models");
 // Create Instance of Express
 var app = express();
 
-// Sets an initial port. We'll use this later in our listener
+// Sets a port
 var PORT = process.env.PORT || 3000;
 
 // Run Morgan for Logging
 app.use(logger("dev"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -21,45 +22,9 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("./public"));
 
-app.get('/api', function(req, res) {
-    camprdb.Topic.findAll({})
-        .then(function(response) {
-            res.json(response)
-        });
-});
+require("./routes/webroutes.js")(app);
 
-app.get("/topics", function(req, res) {
-
-    Topic.find({}, function(err, doc) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(doc);
-        }
-
-    });
-
-});
-
-app.get("/:topic/subtopics", function(req, res) {
-
-    Topic.find({ "name": req.params.topic })
-        .populate("subtopic")
-        .exec(function(err, doc) {
-            if (err) {
-                console.log(error);
-            } else {
-                console.log(doc);
-            }
-        });
-});
-
-// Main "/" Route. This will redirect the user to our rendered React application
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/index.html");
-});
-
-camprdb.sequelize.sync({ force: true }).then(function() {
+camprdb.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT: " + PORT);
     });
