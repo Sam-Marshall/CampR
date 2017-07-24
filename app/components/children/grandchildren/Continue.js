09c73ff48f934d6b9960db9064ecb9eaed96f19a
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-07-24 17:06:53 -0500

    codeacademy link added

--- app/components/children/grandchildren/Continue.js
+++ app/components/children/grandchildren/Continue.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const courseraLink = "https://www.coursera.org/courses?languages=en&query=";
 const edxLink = "https://www.edx.org/course?search_query=";
+let codeAcademyLink;
 
 export default class Continue extends React.Component{
   
@@ -25,11 +26,28 @@ export default class Continue extends React.Component{
   }
 
   render() {
+
+    switch(this.state.topicName){
+      case 'HTML':
+        codeAcademyLink = 'https://www.codecademy.com/courses/html-one-o-one';
+        break;
+      case 'CSS':
+        codeAcademyLink = 'https://www.codecademy.com/courses/css-coding-with-style';
+        break;
+      case 'Javascript':
+        codeAcademyLink = 'https://www.codecademy.com/learn/javascript';
+        break;
+      default:
+        codeAcademyLink = 'https://www.codecademy.com/';
+        break;
+    }
+    
     return (
       <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-phone mdl-color--white resources">
         
         <a href={courseraLink + this.state.topicName + ' ' + this.state.subtopicName} target="_blank"><img className="eduPic" src={"http://www.pekinpubliclibrary.org/wp-content/uploads/2015/03/education-coursera-logo-150x150.png"}/></a>
         <a href={edxLink + this.state.topicName + ' ' + this.state.subtopicName} target="_blank"><img className="eduPic" src={"https://www.edx.org/sites/default/files/theme/edx-logo-header.png"}/></a>
+        <a href={codeAcademyLink} target="_blank"><img className="eduPic" src={"https://p14.zdassets.com/hc/settings_assets/1030455/200272088/1muBAt6udX37m4Ihlik09w-CC_Logotype_Lockup_Blue_Alpha.png"}/></a>
 
       </div>
     );
