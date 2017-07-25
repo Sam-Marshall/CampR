77a933fe9f0e903fe7aea00892bf4e4f8442271f
Angelo <aflorend@gmail.com>
2017-07-24 22:30:06 -0500

    V1 of styled code snippet from DB. Just need to populate with more code.

--- app/components/children/CodeSnippet.js
+++ app/components/children/CodeSnippet.js
@@ -1,27 +1,22 @@
 // React Dependencies
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class CodeSnippet extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
       snippetName: "",
-      snippetCode: `
-      <!DOCTYPE html>
-        <html>
-          <head>
-            <meta charset="utf-8">
-            <title>My test page</title>
-          </head>
-          <body>
-            <p>This is my page</p>
-          </body>
-        </html>`
+      snippetCode: ""
     }
   }
 
   componentDidUpdate() {
+    this.setState({
+      snippetName: this.props.subtopicData[this.props.subtopicId].Snippets[0].name,
+      snippetCode: this.props.subtopicData[this.props.subtopicId].Snippets[0].code
+    });
+    Prism.highlightAll();
   }
 
   render() {
@@ -29,31 +24,31 @@ export default class CodeSnippet extends React.Component{
           <div className="mdl-cell mdl-cell--5-col mdl-cell--12-col-tablet mdl-cell--12-col-phone code-snippet">
             <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
               <div className="mdl-tabs__tab-bar">
-                  <a href={"#Exercise-1-panel"} className="mdl-tabs__tab is-active">Exercise-1</a>
-                  <a href={"#Exercise-2-panel"} className="mdl-tabs__tab">Exercise-2</a>
-                  <a href={"#Exercise-3-panel"} className="mdl-tabs__tab">Exercise-3</a>
+                <a href={"#Exercise-1-panel"} className="mdl-tabs__tab is-active">{this.state.snippetName}</a>
+                <a href={"#Exercise-2-panel"} className="mdl-tabs__tab">{this.state.snippetName}</a>
+                <a href={"#Exercise-3-panel"} className="mdl-tabs__tab">{this.state.snippetName}</a>
               </div>
 
               <div className="mdl-tabs__panel is-active" id="Exercise-1-panel">
                 <pre>
                   <code className="language-markup">
-                  {this.state.snippetCode}
+                    {this.state.snippetCode}
                   </code>
                 </pre>
               </div>
               <div className="mdl-tabs__panel" id="Exercise-2-panel">
-                <ul>
-                  <li>Tywin</li>
-                  <li>Cersei</li>
-                  <li>Jamie</li>
-                  <li>Tyrion</li>
-                </ul>
+                <pre>
+                  <code className="language-markup">
+                    {this.state.snippetCode}
+                  </code>
+                </pre>
               </div>
               <div className="mdl-tabs__panel" id="Exercise-3-panel">
-                <ul>
-                  <li>Viserys</li>
-                  <li>Daenerys</li>
-                </ul>
+                <pre>
+                  <code className="language-markup">
+                    {this.state.snippetCode}
+                  </code>
+                </pre>
               </div>
             </div>
 
