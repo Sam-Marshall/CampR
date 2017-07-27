78dc4f3f9e7d98177b274a84235059026a225552
aflorend <aflorend@gmail.com>
2017-07-27 12:10:19 -0500

    Completed landing page. Will improve, time permitting

--- app/components/children/Landing.js
+++ app/components/children/Landing.js
@@ -18,7 +18,7 @@ export default class Landing extends React.Component {
       landingDiv = (
         <div className="landing-bg">
           <h1 className="landing-title">CAMPR</h1>
-          <h4 className="landing-tagline">Your coding bootcamp site</h4>
+          <h3 className="landing-tagline">Your coding bootcamp site</h3>
           <div className="landing-btn" onClick={(e) => this.handleClick(e)}><span>Let's get started</span></div>
         </div>
       );
